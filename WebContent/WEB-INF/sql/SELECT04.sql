ce46da83569358dedb0b6bedcfb9076731377043
eemin90 <eemin90@naver.com>
2021-05-28 18:04:13 +0900

    SQL

---
+++ WebContent/WEB-INF/sql/SELECT04.sql
@@ -0,0 +1,18 @@
+SELECT CustomerName FROM Customers
+WHERE CustomerID = 1;
+
+SELECT CustomerName, City FROM Customers
+WHERE CustomerID = 1;
+
+SELECT CustomerID, CustomerName, City From Customers
+WHERE CustomerID = 2;
+
+SELECT CustomerID From Customers;
+
+SELECT * FROM Employees
+WHERE EmployeeID = 1;
+
+SELECT DISTINCT City FROM Customers ORDER BY City;
+
+SELECT EmployeeID, LastName, FirstName FROM Employees
+WHERE EmployeeID = 1;

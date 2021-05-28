1c71bd7071947278fd002626048df814f99d7aca
eemin90 <eemin90@naver.com>
2021-05-28 18:31:47 +0900

    14장 데이터베이스 프로그래밍 기초

---
+++ src/ch14/bean/Imployee.java
@@ -0,0 +1,31 @@
+package ch14.bean;
+
+public class Imployee {
+	private int eid;
+	private String lastName;
+	private String firstName;
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+}
